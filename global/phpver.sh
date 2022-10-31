67a6af0e8c0f2d33f38174a8f0830571ccb1391f
Francisco Solis <fran@theprogramsrc.xyz>
2022-10-30 23:20:46 -0300

    Fix for ubuntu

--- global/phpver.sh
+++ global/phpver.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 if [ -f ".phpver" ]; then
-    PHP_VER=$(cat .phpver)
-    BREW_PHP_VERSIONS_CHECK=$(brew list --versions | grep "php.* $PHP_VER")
-    if ! [ "$BREW_PHP_VERSIONS_CHECK" = "" ]; then
-        brew unlink php > /dev/null 2>&1
-        brew link "php@$PHP_VER" --force --overwrite > /dev/null 2>&1
-    fi
+	PHPVER=$(cat .phpver)
+	BREW_PHP_VERSIONS_CHECK=$(brew list --versions | grep "php.*$PHPVER")
+	if ! [ "$BREW_PHP_VERSIONS_CHECK" = "" ]; then
+		brew unlink php > /dev/null 2>&1
+		# Only from 7.4 - 8.1 cuz those are the ones I use, if you want to feel free to add more.
+		# Also I don't use the variable cuz it seems to have issues in ubuntu.
+		if [ "$PHPVER" = "8.1" ]; then
+			brew link php@8.1 > /dev/null 2>&1
+		elif [ "$PHPVER" = "8.0" ]; then
+			brew link php@8.0 > /dev/null 2>&1
+		elif [ "$PHPVER" = "7.4" ]; then
+			brew link php@7.4 > /dev/null 2>&1
+		fi
+	fi
 fi
